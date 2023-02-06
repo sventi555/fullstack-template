ece1645c4a218326beef34faa4beced90aa5a117
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-05 19:47:20 -0500

    chore: add environment setup to client

--- packages/client/server/entrypoint.sh
+++ packages/client/server/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Construct an up to date environment.js file
 echo "window.ENV = {
-   
+  \"apiHost\": \"$VITE_API_HOST\",
 };" > /usr/share/nginx/html/environment.js;
 
 envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
