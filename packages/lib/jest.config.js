285dece54d27351baf694bc811af6a11189639ca
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-05 23:47:12 -0500

    chore: add github ci action

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.cjs.json',
-    },
-  },
   clearMocks: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
