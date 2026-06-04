ef5ddaf4e943a6740e04f491981413dedd59d6f5
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2026-06-04 11:08:32 -0400

    bring back prettier

---
+++ apps/client/prettier.config.js
@@ -0,0 +1,6 @@
+import config from '../../prettier.config.js';
+
+export default {
+  ...config,
+  plugins: [...(config.plugins ?? []), 'prettier-plugin-tailwindcss'],
+};
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ['prettier-plugin-organize-imports'],
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
